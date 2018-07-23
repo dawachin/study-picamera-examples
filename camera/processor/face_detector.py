86b3a06f1095e6b1fb24553ade6f610228717a01
hiroaki ohwada <dawachin@git.com>
2018-07-23 20:32:41 +0900

    no message

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -37,6 +37,7 @@ class FaceDetector(object):
         faces = self.face_cascade.detectMultiScale(gray, 1.3, 3)
 
         # cv2.putText(frame, len(faces))
+        cv2.putText(frame,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
 
         # 取得した座標情報を元に、cv2.rectangleを使ってframe上に
         # 顔の位置を描画する
